0f8c84133d1676211c7978ecf2c618560fbc75d2
moneshvel66 <moneshvel66@gmail.com>
2024-10-16 18:28:11 +0530

    changed home page

--- backend/routes/loginRoute.js
+++ backend/routes/loginRoute.js
@@ -33,32 +33,6 @@ const loginUser = async (req, res) => {
     console.log("hi from error in login route", error);
     return res.status(500).json({ code: -1, msg: "Internel server error", error: err });
   }
-  const user = await login.findOne({ userName });
-  if (!user) {
-    console.log("user not exist");
-    return res.status(400).json({ code: 0, msg: "user didnt exist" });
-  }
-  if (!bcrypt.compare(password, user.password)) {
-    res.status(400).json({ code: 0, msg: "invalid password u fool" });
-  }
-  const token = jwt.sign(
-    { userName: user.userName },
-    process.env.SECRET_KEY,
-    { expiresIn: "15m" }
-  );
-  console.log("user signed in token created succesfully token:", token);
-  const option = {
-    expires: new Date(Date.now() + 2 * 24 * 60 * 60 * 1000),
-    httpOnly: true,
-  }
-  user.password = undefined;
-  user.token = token;
-  res.status(201).json({ code: 1, msg: "User Signed in succesfully", JWT: token, user })
-
-} catch (error) {
-  console.log("hi from error", error);
-  res.status(500).json({ code: -1, msg: "Internel server error" });
-}
 }
 loginRouter.post('/', loginUser);
 module.exports = loginRouter;
