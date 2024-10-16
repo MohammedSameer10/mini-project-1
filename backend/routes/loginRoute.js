463cf5c9eb5bf88a345631ae3520828756724ad1
MohammedSameer10 <mohammedsameer.sm10@gmail.com>
2024-10-16 17:18:36 +0530

    home route changed fully

--- backend/routes/loginRoute.js
+++ backend/routes/loginRoute.js
@@ -1,102 +1,52 @@
-<<<<<<< HEAD
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const loginRouter = express.Router();
-const login = require('../model/logins')
+const login = require('../model/logins');
+
 const loginUser = async (req, res) => {
   try {
     const { userName, password } = req.body;
+
+    // Validation for missing fields
     if (!(userName && password)) {
-      console.log("enter every field");
-      return res.status(400).json({ code: "0", msg: "validation error" });
-=======
-const express=require('express');
-const bcrypt=require('bcryptjs');
-const jwt=require('jsonwebtoken');
-const loginRouter=express.Router();
-const login=require('../model/logins')
-const loginUser=async (req,res)=>{
-    try{
-        const{userName,password}=req.body;
-        if(!(userName&&password)){
-            console.log("enter every field");
-           return res.status(400).json({code:"0",msg:"validation error"});
-        }
-      const user= await login.findOne({userName});
-      if(!user){
-        console.log("user not exist");
-       return res.status(400).json({code:0,msg:"username not found"});
-      }
-      if(password!==user.password){
-       return res.status(400).json({code:0,msg:"password is wrong"});
-      }
-      const token = jwt.sign(
-        {userName:user.userName},
-        process.env.SECRET_KEY,
-        {expiresIn:'2m'}
-      );
-      console.log("user signed in token created succesfully token:",token);
-      
-      user.password=undefined;
-      user.token=token;
-      return res.status(201).json({code:1,msg:"login succesfull",JWT:token})
-      
-    }catch(error){
-       console.log("hi from error in login route",error);
-      return res.status(500).json({code:-1,msg:"Internel server error",error:err});
->>>>>>> 7d42727 (home route changed fully)
+      console.log("Enter all fields");
+      return res.status(400).json({ code: "0", msg: "Validation error: Missing fields" });
     }
+
+    // Find the user by userName
     const user = await login.findOne({ userName });
     if (!user) {
-      console.log("user not exist");
-      return res.status(400).json({ code: 0, msg: "username not found" });
+      console.log("User does not exist");
+      return res.status(400).json({ code: 0, msg: "Username not found" });
     }
-    if (!password === user.password) {
-      return res.status(400).json({ code: 0, msg: "password is wrong" });
+
+    // Compare entered password with stored hashed password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ code: 0, msg: "Incorrect password" });
     }
+
+    // Generate JWT token
     const token = jwt.sign(
       { userName: user.userName },
       process.env.SECRET_KEY,
-      { expiresIn: "15m" }
+      { expiresIn: "15m" } // Token expiry time
     );
-    console.log("user signed in token created succesfully token:", token);
+    console.log("User signed in, token created successfully. Token:", token);
 
+    // Remove password from user object before sending response
     user.password = undefined;
     user.token = token;
-    return res.status(201).json({ code: 1, msg: "login succesfull", JWT: token })
+
+    // Send the response with the token
+    return res.status(201).json({ code: 1, msg: "Login successful", JWT: token });
 
   } catch (error) {
-    console.log("hi from error in login route", error);
-    return res.status(500).json({ code: -1, msg: "Internel server error", error: err });
+    console.log("Error in login route", error);
+    return res.status(500).json({ code: -1, msg: "Internal server error", error });
   }
-  const user = await login.findOne({ userName });
-  if (!user) {
-    console.log("user not exist");
-    return res.status(400).json({ code: 0, msg: "user didnt exist" });
-  }
-  if (!bcrypt.compare(password, user.password)) {
-    res.status(400).json({ code: 0, msg: "invalid password u fool" });
-  }
-  const token = jwt.sign(
-    { userName: user.userName },
-    process.env.SECRET_KEY,
-    { expiresIn: "15m" }
-  );
-  console.log("user signed in token created succesfully token:", token);
-  const option = {
-    expires: new Date(Date.now() + 2 * 24 * 60 * 60 * 1000),
-    httpOnly: true,
-  }
-  user.password = undefined;
-  user.token = token;
-  res.status(201).json({ code: 1, msg: "User Signed in succesfully", JWT: token, user })
+};
 
-} catch (error) {
-  console.log("hi from error", error);
-  res.status(500).json({ code: -1, msg: "Internel server error" });
-}
-}
 loginRouter.post('/', loginUser);
 module.exports = loginRouter;
-
